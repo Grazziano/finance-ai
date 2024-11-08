261969ae6876c6c530faa09213c1e2916c83ed9e
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2024-11-08 09:18:13 -0300

    feat: add navbar component

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,3 +1,14 @@
-const SubscriptionPage = () => {};
+import Navbar from "../_components/navbar";
+
+const SubscriptionPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="flex h-full w-full items-center justify-center">
+        <h1 className="text-2xl font-bold">Subscription</h1>
+      </div>
+    </>
+  );
+};
 
 export default SubscriptionPage;
